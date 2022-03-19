608cd3a45b0f721c64d2e1ed3f4c073f9e9f6a07
yuhsiang237 <umworksite@gmail.com>
2022-03-19 18:23:32 +0800

    upload repo

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EndlessTaskAsync
+{
+    class Program
+    {
+        /// <summary>
+        /// 主程式
+        /// </summary>
+        /// <returns></returns>
+        public static async Task Main()
+        {
+            var endlessTask1 = EndlessTask(1);
+            var endlessTask2 = EndlessTask(2);
+
+            await PreventToStop();
+        }
+
+        /// <summary>
+        /// 製作無止盡的 while task
+        /// </summary>
+        /// <param name="number">編號</param>
+        /// <returns></returns>
+        private static Task EndlessTask(int number)
+        {
+            Console.WriteLine("Start EndlessTask" + number);
+            var endlessTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    Console.WriteLine("EndlessTask" + number);
+                    await Task.Delay(100);
+                }
+            });
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 避免視窗關閉，使用ReadKey();
+        /// </summary>
+        /// <returns></returns>
+        private static Task PreventToStop()
+        {
+            Console.ReadKey();
+            return Task.CompletedTask;
+        }
+    }
+}
